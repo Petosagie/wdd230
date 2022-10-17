553d4f6398aa3b600c7ff8e5c0e151476bf97ea7
petosagie <petosagie@gmail.com>
2022-10-17 04:58:45 +0100

    updated html and css

---
+++ chamber/scripts/banner.js
@@ -0,0 +1,8 @@
+const weekday = new Date().getDay();
+if (weekday == 1) {
+    document.querySelector('.banner').style.display='block';
+}
+
+if (weekday == 2) {
+    document.querySelector('.banner').style.display='block';
+}
