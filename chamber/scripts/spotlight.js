2e03333711daae4d10696e439f8df083bb2b1f0f
petosagie <petosagie@gmail.com>
2022-11-30 14:50:58 +0100

    update

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,77 +1,38 @@
-const requestURL = 'https://petosagie.github.io/wdd230/chamber/data.json';
-const aside = document.querySelector('.aside');
-
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+const spotlightRequestURL = 'data.json';
 
 
+fetch(spotlightRequestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+        const spotlightCompanies = jsonObject["directory"];
+        // jsonObject.companies
+        let lenght = Object.keys(spotlightCompanies).length;
+        console.log(lenght);
+        for (let i = 1; i <= lenght; i++) {
+            if (spotlightCompanies[i].membership === 'gold' || spotlightCompanies[i].membership === 'silver') {
 
-fetch(requestURL)
-  .then(function (response) {
-    console.log(response.json());
-  })
-  .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const directory = jsonObject;
-    // const businesses = directory.filter((business) => business.membership == 'Gold');
-    console.log(directory);
-    // create_cards(businesses);
-  });
+            for (let i = 1; i < 4; i++) {
+            
+                const spotlightCard = document.querySelector(`.spotlight_${i}`);
+                console.log(Math.random());
+                spotlightCard.innerHTML = displayspotlightCompanies(spotlightCompanies[Math.floor(Math.random() * lenght)]);
+                console.log(spotlightCard);
+                console.log('.spotlight_' + i);
+            }
+        }}
 
+        // let spotlightLenght = object.keys( spotlightCompanies.lenght)
+        // spotlightCompanies.forEach(displayspotlightCompanies);
+    });
 
-let spotlights = [];
-
-function create_cards(businesses){
-    let max = businesses.length;
-
-    
-    while(spotlights.length < 3){
-
-        let random = Math.floor(Math.random()*max);
-
-        if(!spotlights.includes(random)){
-            spotlights.push(random);
-            create_card(businesses[random]);
-        }
-
-    }
-
-    aside.lastChild.classList.add('spotlight3');
-
-}
-
-
-function create_card(business){
-    
-
-    let div = document.createElement('div');
-    let div2 = document.createElement('div');
-    div.setAttribute('class', 'card');
-
-    let img = document.createElement('img');
-    img.setAttribute('src', business.logo);
-    img.setAttribute('alt', business.name + " " + business.membership + " member");
-    img.setAttribute('loading', 'lazy');
-
-
-    let h3 = document.createElement('h3');
-    h3.textContent = business.name;
-
-    let p1 = document.createElement('p');
-    p1.textContent = business.phone;
-
-  
-    let a = document.createElement('a');
-    a.setAttribute('href', business.site);
-    a.textContent = business.site;
-
-    div2.appendChild(img);
-    div2.classList.add('div-height-50');
-    div.appendChild(div2);
-    div.appendChild(h3);
-    div.appendChild(p1);
-
-    div.appendChild(a);
-
-    aside.appendChild(div);
-
+function displayspotlightCompanies(companie) {
+    return `<h2>${companie.name}</h2>
+            <img src="${companie.logo}" alt="">
+            <hr>
+            <p>${companie.address}</p>
+            <p>${companie.website}</p>
+            <p>${companie.phonenumber}</p>`;
 }
